a8dbb2d3e40026095efffbb342d393175f959432
shiwamanami <shiwa.adisign@gmail.com>
2025-09-19 10:44:32 +0900

    first commit

---
+++ src/react-app-env.d.ts
@@ -0,0 +1,36 @@
+/// <reference types="react-scripts" />
+
+declare module '*.svg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.png' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.jpeg' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.gif' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.bmp' {
+  const content: string;
+  export default content;
+}
+
+declare module '*.tiff' {
+  const content: string;
+  export default content;
+}
